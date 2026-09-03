611c694807ccc2049315fbeb67062e3580f61fc8
zhaoxh <zhaoxh@jiahejoy.com>
2026-09-03 18:23:20 +0800

    发布针架豆子落点修复

--- main.3a04e.js
+++ main.3a04e.js
@@ -117,7 +117,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.9fdb6.js');
+        require('src/settings.e05fb.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -125,7 +125,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.9fdb6.js');
+        require('src/settings.e05fb.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');

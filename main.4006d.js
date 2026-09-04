74794f374cf3a24b82619c7559c25f1246a80002
zhaoxh <zhaoxh@jiahejoy.com>
2026-09-04 15:02:31 +0800

    rebuild web-mobile from PR2 @798663a: queue-full tip + increase-queue, level fallback, texture shrink, no autofullscreen

--- main.4006d.js
+++ main.4006d.js
@@ -117,7 +117,7 @@ window.boot = function () {
 if (window.jsb) {
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
-        require('src/settings.b3730.js');
+        require('src/settings.2c008.js');
         require('src/cocos2d-runtime.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
@@ -125,7 +125,7 @@ if (window.jsb) {
         require('jsb-adapter/engine/index.js');
     }
     else {
-        require('src/settings.b3730.js');
+        require('src/settings.2c008.js');
         require('src/cocos2d-jsb.js');
         if (CC_PHYSICS_BUILTIN || CC_PHYSICS_CANNON) {
             require('src/physics.js');
